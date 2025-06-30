3c417d0c9d5c85ad3c20a1560bb7e557ebbe4891
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-06-30 15:07:08 +0530

    ✅ `Logger Integration`: Implemented.

---
+++ backend/src/constants.js
@@ -0,0 +1,2 @@
+export const DB_NAME = '';
+export const COLLECTION_NAME = 'portfolio';
---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+import 'dotenv/config';
+
+import { app } from './app.js';
+import morgan from 'morgan';
+import logger from './utils/logger.js';
+
+const PORT = process.env.PORT || 3000;
+const morganFormat = ':method :url :status :response-time ms';
+
+console.log('🚀 ~ PORT:', PORT);
+
+//! Middlewares
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(' ')[0],
+          url: message.split(' ')[1],
+          status: message.split(' ')[2],
+          responseTime: message.split(' ')[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.status(201).json({
+    success: true,
+    message: 'Server is live',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
