a1d478b57f951daebce43c1e7727e8e2527d7976
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-06-30 19:20:19 +0530

    ✅ `Application Deployment`: Trying to solve the "serverless function issue"

---
+++ backend/src/index.js
@@ -0,0 +1,51 @@
+import 'dotenv/config';
+
+import { app } from './app.js';
+import morgan from 'morgan';
+import logger from './utils/logger.js';
+import connectDB from './db/index.js';
+import { appRouter } from './routes/appRoutes.js';
+import { authRouter } from './routes/authRoutes.js';
+import { adminRouter } from './routes/adminRoutes.js';
+import { authMiddleware } from './middlewares/authMiddleware.js';
+
+const PORT = process.env.PORT || 3000;
+const morganFormat = ':method :url :status :response-time ms';
+
+//! Middlewares
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(' ')[0],
+          url: message.split(' ')[1],
+          status: message.split(' ')[2],
+          responseTime: message.split(' ')[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  }),
+);
+
+app.use('/api', appRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/admin', authMiddleware, adminRouter);
+
+app.get('/', (req, res) => {
+  res.status(201).json({
+    success: true,
+    message: 'Server is live',
+  });
+});
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('Mongo Db Error: ', err);
+  });
